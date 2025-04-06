5391b2f6ed75e7d4d65f3781bf00c6aef671778e
Sanzid Ahmed <sanzidahmed313@gmail.com>
2025-04-06 16:06:57 +0600

    Add files via upload

---
+++ Pattern problems/Questions from simplilearn/Pattern 4 Mirrored Right-Angle Triangle by separate function.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void pattern (int n);
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    pattern (n);
+
+    return 0;
+}
+
+void pattern (int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = i; j < n * 2 - i - 2; j++)
+        {
+            printf(" ");
+        }
+
+        for(int j = 0; j < i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
