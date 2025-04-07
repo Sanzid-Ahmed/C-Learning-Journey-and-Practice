fc1c64bb79e32ee123e6620efd4099d22479949d
Sanzid Ahmed <sanzidahmed313@gmail.com>
2025-04-07 16:37:02 +0600

    Update Pattern 4 Mirrored Right-Angle Triangle by for loop.c

--- Pattern problems/Questions from simplilearn/Pattern 4 Mirrored Right-Angle Triangle by for loop.c
+++ Pattern problems/Questions from simplilearn/Pattern 4 Mirrored Right-Angle Triangle by for loop.c
@@ -7,22 +7,16 @@ int main()
 
     for(int i = 0; i < n; i++)
     {
-        for(int j = i; j < n * 5 - i * 4 - 5; j++)
+        for(int j = i; j < n - 1; j++)
         {
-            printf(" ");
+            printf("  ");
         }
 
         for(int j = 0; j < i; j++)
         {
-            printf("*    ");
-            /* Here I use 4 space thats why I multiply 4 with i.
-            Then multiply 4 + 1 = 5 with n and minus 5 from i*4.*/
-
-
-            /* if i assume space after * as s
-            Then condition n * (s + 1) - i * s - (s + 1).*/
+            printf("* ");
         }
-        printf("\n\n");
+        printf("\n");
     }
     return 0;
 }
